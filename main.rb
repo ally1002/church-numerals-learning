c5246b2b5a65eef26bdfc1d8e9b35c90a342c31e
aly <alyssonrosa1@gmail.com>
2025-09-04 13:26:41 -0300

    sum

--- main.rb
+++ main.rb
@@ -3,13 +3,19 @@
 
 IDENTITY = ->(x) { x }
 SUCESSOR = ->(n) { ->(f) { ->(x) { f.call(n.call(f).call(x)) } }}
+SUM = ->(m,n) { ->(f) { ->(x) { m.call(f).call(n.call(f).call(x)) } }}
 
 ZERO = ->(f) { IDENTITY }
 ONE  = ->(f) { ->(x) { f.call(x) } }
 TWO  = ->(f) { ->(x) { f.call(f.call(x)) } }
 THREE  = SUCESSOR.call(TWO)
+FOUR  = SUCESSOR.call(THREE)
+FIVE  = SUCESSOR.call(FOUR)
+SIX  = SUCESSOR.call(FIVE)
+SEVEN  = SUCESSOR.call(SIX)
+EIGHT  = SUCESSOR.call(SEVEN)
+NINE  = SUCESSOR.call(EIGHT)
 
 def to_number(n) = n.call(->(i) { i + 1 }).call(0)
 
-result = to_number(THREE)
-pp result
+pp to_number(MULT.call(TWO, ZERO))
